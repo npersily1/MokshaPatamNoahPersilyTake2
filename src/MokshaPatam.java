2e1dc7fe6798a064229d6e42ff84fa778896ca3f
npersily1 <noah.persily@menloschool.org>
2024-09-03 19:38:28 -0700

    Worklog and Helper methods

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
@@ -16,6 +18,27 @@ public class MokshaPatam {
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
 
+           int count = ladderDifference(ladders);
+        ArrayList<int[]> ladderList = new ArrayList<int[]>();
         return 0;
     }
+    public static void lowerBound(int[][]ladders, ArrayList<int[]> a, int upperBound) {
+
+
+
+    }
+    // Returns index of ladder with the biggest difference
+    public static int ladderDifference(int[][] ladders) {
+        int greatestDiff = 0;
+        int count = 0;
+        for (int i = 0; i < ladders.length;i++) {
+            if (greatestDiff < ladders[i][1] - ladders[i][0]) {
+                greatestDiff = ladders[i][1] - ladders[i][0];
+                count = i;
+            }
+        }
+        return count;
+    }
+    // add a method that checks if a ladder is in bounds. (useful for recursion)
+
 }
